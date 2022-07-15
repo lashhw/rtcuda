335fd3a22eeba572ce9a2db4cb8738a06c013b58
Jacky Huang <lashhw81@gmail.com>
2022-07-15 22:34:31 +0800

    remove redundant __forceinline__

--- Vec3.h
+++ Vec3.h
@@ -23,109 +23,109 @@ struct Vec3 {
     float x, y, z;
 };
 
-__host__ __device__ __forceinline__ Vec3 operator+(const Vec3 &v1, const Vec3 &v2) {
+__host__ __device__ Vec3 operator+(const Vec3 &v1, const Vec3 &v2) {
     return Vec3(v1.x+v2.x, v1.y+v2.y, v1.z+v2.z);
 }
 
-__host__ __device__ __forceinline__ Vec3 operator-(const Vec3 &v1, const Vec3 &v2) {
+__host__ __device__ Vec3 operator-(const Vec3 &v1, const Vec3 &v2) {
     return Vec3(v1.x-v2.x, v1.y-v2.y, v1.z-v2.z);
 }
 
-__host__ __device__ __forceinline__ Vec3 operator*(const Vec3 &v1, const Vec3 &v2) {
+__host__ __device__ Vec3 operator*(const Vec3 &v1, const Vec3 &v2) {
     return Vec3(v1.x*v2.x, v1.y*v2.y, v1.z*v2.z);
 }
 
-         __device__ __forceinline__ Vec3 operator/(const Vec3 &v1, const Vec3 &v2) {
+         __device__ Vec3 operator/(const Vec3 &v1, const Vec3 &v2) {
     return Vec3(__fdividef(v1.x, v2.x), __fdividef(v1.y, v2.y), __fdividef(v1.z, v2.z));
 }
 
-__host__ __device__ __forceinline__ Vec3 operator*(const Vec3 &v, float t) {
+__host__ __device__ Vec3 operator*(const Vec3 &v, float t) {
     return Vec3(v.x * t, v.y * t, v.z * t);
 }
 
-__host__ __device__ __forceinline__ Vec3 operator*(float t, const Vec3 &v) {
+__host__ __device__ Vec3 operator*(float t, const Vec3 &v) {
     return v * t;
 }
 
-__host__ __device__ __forceinline__ Vec3 operator/(const Vec3 &v, float t) {
+__host__ __device__ Vec3 operator/(const Vec3 &v, float t) {
     float inv_t = 1.0f / t;
     return v * inv_t;
 }
 
-__host__ __device__ __forceinline__ float dot (const Vec3 &v1, const Vec3 &v2) {
+__host__ __device__ float dot (const Vec3 &v1, const Vec3 &v2) {
     return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z;
 }
 
-__host__ __device__ __forceinline__ Vec3 cross(const Vec3 &v1, const Vec3 &v2) {
+__host__ __device__ Vec3 cross(const Vec3 &v1, const Vec3 &v2) {
     return Vec3(v1.y*v2.z - v1.z*v2.y,
                 v1.z*v2.x - v1.x*v2.z,
                 v1.x*v2.y - v1.y*v2.x);
 }
 
-__host__ __device__ __forceinline__ Vec3 reflect(const Vec3 &v, const Vec3 &unit_normal) {
+__host__ __device__ Vec3 reflect(const Vec3 &v, const Vec3 &unit_normal) {
     return v - 2.0f*dot(v, unit_normal)*unit_normal;
 }
 
-__host__ __device__ __forceinline__ Vec3 refract(const Vec3 &unit_v, const Vec3 &unit_normal, double eta_ratio) {
+__host__ __device__ Vec3 refract(const Vec3 &unit_v, const Vec3 &unit_normal, double eta_ratio) {
     double cos_theta = -dot(unit_v, unit_normal);
     Vec3 v_parallel = eta_ratio * (unit_v+cos_theta*unit_normal);
     Vec3 v_perp = -sqrtf(1.0f-v_parallel.length_squared()) * unit_normal;
     return v_parallel + v_perp;
 }
 
-__host__ __device__ __forceinline__ Vec3& Vec3::operator+=(const Vec3 &v2) {
+__host__ __device__ Vec3& Vec3::operator+=(const Vec3 &v2) {
     x += v2.x;
     y += v2.y;
     z += v2.z;
     return *this;
 }
 
-__host__ __device__ __forceinline__ Vec3& Vec3::operator-=(const Vec3 &v2) {
+__host__ __device__ Vec3& Vec3::operator-=(const Vec3 &v2) {
     x -= v2.x;
     y -= v2.y;
     z -= v2.z;
     return *this;
 }
 
-__host__ __device__ __forceinline__ Vec3& Vec3::operator*=(const Vec3 &v2) {
+__host__ __device__ Vec3& Vec3::operator*=(const Vec3 &v2) {
     x *= v2.x;
     y *= v2.y;
     z *= v2.z;
     return *this;
 }
 
-         __device__ __forceinline__ Vec3& Vec3::operator/=(const Vec3 &v2) {
+         __device__ Vec3& Vec3::operator/=(const Vec3 &v2) {
     x = __fdividef(x, v2.x);
     y = __fdividef(y, v2.y);
     z = __fdividef(z, v2.z);
     return *this;
 }
 
-__host__ __device__ __forceinline__ Vec3& Vec3::operator*=(float t) {
+__host__ __device__ Vec3& Vec3::operator*=(float t) {
     x *= t;
     y *= t;
     z *= t;
     return *this;
 }
 
-__host__ __device__ __forceinline__ Vec3& Vec3::operator/=(float t) {
+__host__ __device__ Vec3& Vec3::operator/=(float t) {
     float inv_t = 1.0f / t;
     return operator*=(inv_t);
 }
 
-__host__ __device__ __forceinline__ Vec3 Vec3::unit_vector() const {
+__host__ __device__ Vec3 Vec3::unit_vector() const {
     float inv_len = 1.0f / this->length();
     return Vec3(x * inv_len, y * inv_len, z * inv_len);
 }
 
-__host__ __device__ __forceinline__ void Vec3::unit_vector_inplace() {
+__host__ __device__ void Vec3::unit_vector_inplace() {
     float inv_len = 1.0f / this->length();
     x = x * inv_len;
     y = y * inv_len;
     z = z * inv_len;
 }
 
-__host__ __device__ __forceinline__ void Vec3::sqrt_inplace() {
+__host__ __device__ void Vec3::sqrt_inplace() {
     x = sqrtf(x);
     y = sqrtf(y);
     z = sqrtf(z);
