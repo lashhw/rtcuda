6618f251e46d410df39fab7f6d503be099715ca8
Jacky Huang <lashhw81@gmail.com>
2022-07-22 22:04:16 +0800

    vec3 minor update

--- vec3.cuh
+++ vec3.cuh
@@ -5,20 +5,20 @@ struct Vec3 {
     __host__ __device__ Vec3() { }
     __host__ __device__ Vec3(float x, float y, float z) : x(x), y(y), z(z) { }
 
-    __host__ __device__ __forceinline__ Vec3 operator-() const { return Vec3(-x, -y, -z); }
+    __host__ __device__ Vec3 operator-() const { return Vec3(-x, -y, -z); }
 
-    __host__ __device__ __forceinline__ Vec3& operator+=(const Vec3 &v2);
-    __host__ __device__ __forceinline__ Vec3& operator-=(const Vec3 &v2);
-    __host__ __device__ __forceinline__ Vec3& operator*=(const Vec3 &v2);
-             __device__ __forceinline__ Vec3& operator/=(const Vec3 &v2);
-    __host__ __device__ __forceinline__ Vec3& operator*=(float t);
-    __host__ __device__ __forceinline__ Vec3& operator/=(float t);
+    __host__ __device__ Vec3& operator+=(const Vec3 &v2);
+    __host__ __device__ Vec3& operator-=(const Vec3 &v2);
+    __host__ __device__ Vec3& operator*=(const Vec3 &v2);
+             __device__ Vec3& operator/=(const Vec3 &v2);
+    __host__ __device__ Vec3& operator*=(float t);
+    __host__ __device__ Vec3& operator/=(float t);
 
-    __host__ __device__ __forceinline__ float length_squared() const { return x*x + y*y + z*z; }
-    __host__ __device__ __forceinline__ float length() const { return sqrtf(length_squared()); }
-    __host__ __device__ __forceinline__ Vec3 unit_vector() const;
-    __host__ __device__ __forceinline__ void unit_vector_inplace();
-    __host__ __device__ __forceinline__ void sqrt_inplace();
+    __host__ __device__ float length_squared() const { return x * x + y * y + z * z; }
+    __host__ __device__ float length() const { return sqrtf(x * x + y * y + z * z); }
+    __host__ __device__ Vec3 unit_vector() const;
+    __host__ __device__ void unit_vector_inplace();
+    __host__ __device__ void sqrt_inplace();
 
     float x, y, z;
 };
@@ -44,32 +44,32 @@ __host__ __device__ Vec3 operator*(const Vec3 &v, float t) {
 }
 
 __host__ __device__ Vec3 operator*(float t, const Vec3 &v) {
-    return v * t;
+    return Vec3(v.x * t, v.y * t, v.z * t);
 }
 
 __host__ __device__ Vec3 operator/(const Vec3 &v, float t) {
-    float inv_t = 1.0f / t;
-    return v * inv_t;
+    float inv_t = 1.f / t;
+    return Vec3(v.x * inv_t, v.y * inv_t, v.z * inv_t);
 }
 
 __host__ __device__ float dot (const Vec3 &v1, const Vec3 &v2) {
-    return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z;
+    return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
 }
 
 __host__ __device__ Vec3 cross(const Vec3 &v1, const Vec3 &v2) {
-    return Vec3(v1.y*v2.z - v1.z*v2.y,
-                v1.z*v2.x - v1.x*v2.z,
-                v1.x*v2.y - v1.y*v2.x);
+    return Vec3(v1.y * v2.z - v1.z * v2.y,
+                v1.z * v2.x - v1.x * v2.z,
+                v1.x * v2.y - v1.y * v2.x);
 }
 
 __host__ __device__ Vec3 reflect(const Vec3 &v, const Vec3 &unit_normal) {
-    return v - 2.0f*dot(v, unit_normal)*unit_normal;
+    return v - 2.f * dot(v, unit_normal) * unit_normal;
 }
 
 __host__ __device__ Vec3 refract(const Vec3 &unit_v, const Vec3 &unit_normal, double eta_ratio) {
     double cos_theta = -dot(unit_v, unit_normal);
     Vec3 v_parallel = eta_ratio * (unit_v+cos_theta*unit_normal);
-    Vec3 v_perp = -sqrtf(1.0f-v_parallel.length_squared()) * unit_normal;
+    Vec3 v_perp = -sqrtf(1.f-v_parallel.length_squared()) * unit_normal;
     return v_parallel + v_perp;
 }
 
@@ -109,20 +109,23 @@ __host__ __device__ Vec3& Vec3::operator*=(float t) {
 }
 
 __host__ __device__ Vec3& Vec3::operator/=(float t) {
-    float inv_t = 1.0f / t;
-    return operator*=(inv_t);
+    float inv_t = 1.f / t;
+    x *= inv_t;
+    y *= inv_t;
+    z *= inv_t;
+    return *this;
 }
 
 __host__ __device__ Vec3 Vec3::unit_vector() const {
-    float inv_len = 1.0f / this->length();
+    float inv_len = 1.f / this->length();
     return Vec3(x * inv_len, y * inv_len, z * inv_len);
 }
 
 __host__ __device__ void Vec3::unit_vector_inplace() {
-    float inv_len = 1.0f / this->length();
-    x = x * inv_len;
-    y = y * inv_len;
-    z = z * inv_len;
+    float inv_len = 1.f / this->length();
+    x *= inv_len;
+    y *= inv_len;
+    z *= inv_len;
 }
 
 __host__ __device__ void Vec3::sqrt_inplace() {
