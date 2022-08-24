9fbd9eb46bb4657c2f058141341c772914958c0d
Jacky Huang <lashhw81@gmail.com>
2022-08-24 12:32:31 +0800

    int octant[3] -> int3 octant

--- aabb_intersector.cuh
+++ aabb_intersector.cuh
@@ -6,7 +6,7 @@ struct AABBIntersector {
 
     __device__ bool intersect(const BoundingBox &bbox, float &entry) const;
 
-    int octant[3];
+    int3 octant;
     Vec3 inverse_direction;
     Vec3 scaled_origin;
 };
@@ -22,14 +22,14 @@ __device__ AABBIntersector::AABBIntersector(const Ray &ray) : octant { ray.unit_
 }
 
 __device__ bool AABBIntersector::intersect(const BoundingBox &bbox, float &entry) const {
-    float entry_x = inverse_direction.x * bbox.bounds[0 + octant[0]] + scaled_origin.x;
-    float entry_y = inverse_direction.y * bbox.bounds[2 + octant[1]] + scaled_origin.y;
-    float entry_z = inverse_direction.z * bbox.bounds[4 + octant[2]] + scaled_origin.z;
+    float entry_x = inverse_direction.x * bbox.bounds[0 + octant.x] + scaled_origin.x;
+    float entry_y = inverse_direction.y * bbox.bounds[2 + octant.y] + scaled_origin.y;
+    float entry_z = inverse_direction.z * bbox.bounds[4 + octant.z] + scaled_origin.z;
     entry = fmaxf(entry_x, fmaxf(entry_y, entry_z));
 
-    float exit_x = inverse_direction.x * bbox.bounds[1 - octant[0]] + scaled_origin.x;
-    float exit_y = inverse_direction.y * bbox.bounds[3 - octant[1]] + scaled_origin.y;
-    float exit_z = inverse_direction.z * bbox.bounds[5 - octant[2]] + scaled_origin.z;
+    float exit_x = inverse_direction.x * bbox.bounds[1 - octant.x] + scaled_origin.x;
+    float exit_y = inverse_direction.y * bbox.bounds[3 - octant.y] + scaled_origin.y;
+    float exit_z = inverse_direction.z * bbox.bounds[5 - octant.z] + scaled_origin.z;
     float exit = fminf(exit_x, fminf(exit_y, exit_z));
 
     return entry <= exit;
